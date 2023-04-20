08570d16fdd1444d6da524d56762d58a23eba4a4
Your Name <you@example.com>
2023-04-19 18:24:46 -0700

    removed score cam

--- slowfast/visualization/weight_calcs.py
+++ slowfast/visualization/weight_calcs.py
@@ -35,40 +35,6 @@ def get_model_weights(inputs = None, grads = None, activations = None, method = 
         projection = projection.reshape(activations.shape[:-1])
         return(torch.FloatTensor(projection).cuda())
     
-    elif method == 'score_cam':
-        #TODO
-        with torch.no_grad():
-            pdb.set_trace()
-            upsample = torch.nn.UpsamplingBilinear2d(size=inputs.shape[-2:])
-            activation_tensor = torch.from_numpy(activations)
-            if self.cuda:
-                activation_tensor = activation_tensor.cuda()
-    
-            upsampled = upsample(activation_tensor)
-
-            maxs = upsampled.view(upsampled.size(0), upsampled.size(1), -1).max(dim=-1)[0]
-            mins = upsampled.view(upsampled.size(0), upsampled.size(1), -1).min(dim=-1)[0]
-
-            maxs, mins = maxs[:, :, None, None], mins[:, :, None, None]
-            upsampled = (upsampled - mins) / (maxs - mins)
-
-            input_tensors = inputs[:, None, :, :] * upsampled[:, :, None, :, :]
-
-            if hasattr(self, "batch_size"):
-                BATCH_SIZE = self.batch_size
-            else:
-                BATCH_SIZE = 16
-            scores = []
-            for target, tensor in zip(targets, input_tensors):
-                for i in tqdm.tqdm(range(0, tensor.size(0), BATCH_SIZE)):
-                    batch = tensor[i: i + BATCH_SIZE, :]
-                    outputs = [target(o).cpu().item()
-                               for o in self.model(batch)]
-                    scores.extend(outputs)
-            scores = torch.Tensor(scores)
-            scores = scores.view(activations.shape[0], activations.shape[1])
-            weights = torch.nn.Softmax(dim=-1)(scores).numpy()
-            return weights
 
     elif method == 'xgrad_cam':
         activations = np.array(activations.cpu())
